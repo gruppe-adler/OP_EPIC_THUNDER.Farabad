9a72af10c90c7cb5a31e44dd51db6d0d94d57d9a
nomisum <nomisum@gmail.com>
2026-04-30 22:55:33 +0200

    Initial commit

---
+++ USER/missionInfo.hpp
@@ -0,0 +1,10 @@
+/*
+*   Legt allgemeine Information über die Mission fest.
+*/
+
+author = "Niggo";                                               // Missionsersteller
+onLoadName = "Quick Sand";                                                   // Name der Mission
+onLoadMission = "";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
+loadScreen = "data\loadpic.paa";                                                // Ladebild
+overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
+overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
